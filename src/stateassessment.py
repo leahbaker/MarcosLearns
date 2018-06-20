b116937dea85bd59e180c53a299e67f9e308332f
Leah <ldenham828@gmail.com>
2018-06-20 23:46:39 +0000

    moved the files around a little to have main.py running everything

---
+++ src/stateassessment.py
@@ -0,0 +1,9 @@
+# ========================================================================================
+# Marcos first assignment! Loosely structured because I'm making this up as I go along.
+# 
+# Instructions in README, you'll want to read that first.
+#
+# Run your code by calling "python main.py" from the bash console only, you shouldn't touch any file but this one
+# ========================================================================================
+
+print "tester"
