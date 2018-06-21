c5ea5e858fca0a05bb514343b23fab440664d876
Leah <ldenham828@gmail.com>
2018-06-21 01:13:48 +0000

    problem set up, go marco go!

--- src/stateassessment.py
+++ src/stateassessment.py
@@ -3,7 +3,42 @@
 # 
 # Instructions in README, you'll want to read that first.
 #
-# Run your code by calling "python main.py" from the bash console only, you shouldn't touch any file but this one
+# ~~~!!!PROJECT BUILD ASCII EVERYTHING ART!!!~~~
+#
+# This project is going to require using all the things you have learned so far.  Because Im not actually the worst, heres a short list:
+#
+# Ex. Data types, Operators, Functions, If statements, While loops, Lists, Indexing or Slicing, Operations on lists
+#
+# I'm going to give you a string of god knows what (don't go check in main.py, try and think of what might be in it). You're going to create the methods that do all these lovely things with it.
+#
+# HINT 1: Remember you can print out lists and strings and other properties if you are questioning yourself
 # ========================================================================================
 
-print "tester"
+VALID = FALSE
+CURRENT_LIST = []
+
+#LEAH: First, you want to validate the string's contents. It can be anything, all you know is that it is a string. If the string "passes" your tests, flip VALID to true. Else it can remain false, and then return that value to use in the next method.
+def validate_string(str):
+    pass
+
+
+#LEAH: Put your string into a list! Specifically, the final value CURRENT_LIST I have made for you above that starts as an empty list and will hold whatever the current string you are using. One element per list spot. Return the list. I know there are cheat ways to do this, use logic :) Make sure to check the valid boolean first! If the boolean isnt valid, return a print statement that says "Leah is a super mean teacher". Otherwise, return your list (CURRENT_LIST).
+def str_to_list(bool, str):
+    pass
+
+
+def create_box(height, width, char):
+    pass
+
+
+def create_triangle(height, width, char):
+    pass
+
+
+
+def create_box(list):
+    pass
+
+
+def create_marcos_choice(height, width, char):
+    pass
