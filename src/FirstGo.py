c3eddf8ae71f7b15c3e36e78e8cffa08ac7a92be
Leah <ldenham828@gmail.com>
2018-06-26 02:58:11 +0000

    Marcos' attempt 1 and my next project for him. Instruction for his next objective can be found in Attempt1Format.md

---
+++ src/FirstGo.py
@@ -0,0 +1,59 @@
+# ======================== 
+# NEW CODE BASED ON COMMENTS
+# Ignore everything else. We are finding out what the heck this string is and where it comes from and what to do with it. Thats it! 
+# Do these IN ORDER and do not move on until the previous one is working. They build on each other. 
+# ========================
+
+#TODO #1: Im going to give you three strings. Use validate to just print out the value of those strings.
+
+leahString1 = "LEAH"
+leahString2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mi orci, facilisis scelerisque urna et, imperdiet elementum massa."
+leahString3 = "* Im a Su Per WeirD ! $ St>ing :)"
+
+def validate_string(str):
+    pass #print the string
+
+#call function 3 times, once with each string. check out the strings printed! 
+
+
+# ========================
+
+#TODO #2: This is going to print true or false based on whether or not the string is "true" or not.
+
+leahString1 = "true"
+leahString2 = "false"
+leahString3 = "True"
+
+def validate_string2(str):
+    pass #return true if string is "true" and false otherwise
+
+validate_string2(leahString1) #should print true! 
+validate_string2(leahString2) #should print false
+validate_string2(leahString3) #should print false
+
+
+# ========================
+
+#TODO #3: Think of 4 more things that might be true of a string that would fail to make ASCII art or make it icky
+
+#WHY I SUCK FOR ASCII ART: 
+badString1 = "T H I S H A S S P A C E S E V E R Y W H E R E"
+
+#WHY I SUCK FOR ASCII ART: 
+badString2 = "TODO"
+
+#WHY I SUCK FOR ASCII ART: 
+badString3 = "TODO"
+
+#WHY I SUCK FOR ASCII ART: 
+badString4 = "TODO"
+
+#WHY I SUCK FOR ASCII ART: 
+badString5 = "TODO"
+
+
+# ========================
+
+#TODO #4: Given the information that the user will be typing in a string to use to make art, why might we want to "validate it" before trying to turn it into art?
+
+#ANSWER: 
