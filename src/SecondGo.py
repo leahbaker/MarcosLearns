684bbde8b727f19bc94fb0734c516513f9f0c878
Leah <ldenham828@gmail.com>
2018-07-04 03:08:49 +0000

    marcos second pass with my answers filled in. third exercise coming in the next two days, im mentally dead atm

--- src/SecondGo.py
+++ src/SecondGo.py
@@ -8,9 +8,13 @@
 # I went to PythonTutor and visualized the code. Still don't get why I'm getting the None value, cause, you know, I'm zpecial.
 # Asked for help in PythoTutor. The way to not get None is to replace print by return. Funtions use return statements. If there's no reture, there's no (None) output. (I'ts easy how one forgets basic stuff, of course I knew this, its just that the big picture overwhelms me)
 
+# Leah: That's exactly why you write this stuff out. You got it, you just need confidence you got it! Its exactly that. The goal of a function is to return something, NOT to display or print something. Most of the time you dont actually want functions you write trying to spit out values places, thats why learning testing is so important.  Lots of things are invisibile and can overwhelm you if you dont test.  Youd be shocked how many print statments (or the other language equivalents) there are floating around production code in companies. I use them all the time, just to see whats going on in the background to see why things are going wrong.  So yes, the short answer is that functions WANT to return something. You can use "pass" as the body of a funtion you dont want to fill in yet to avoid this. Otherwise, MOST of the time you should be returning something. 
+
 # ========================
 
 #TODO #2: Rewrite the function so that it shows you the string value but only prints one thing every time you run it, no more none. 
+
+# Leah: SEE. You know this. :) 
 
 leahString1 = "LEAH"
 leahString2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mi orci, facilisis scelerisque urna et, imperdiet elementum massa."
@@ -42,6 +46,7 @@ def contains_spaces(str):
         return False
 # Is this correct? I feel like its the same I did last time. Maybe this time I'm using '_' correctly. 
 # ASKED FOR HELP IN PYTHONTUTOR AGAIN (these guys are amazing) --> '' is empry string. '_' is space. Got it! (again, it's so obvious, it slips away from my mind :S)
+# Leah: See? You TOTALLY knew that! I KNOW you knew that! Remember, if it seems heavy, stop and take a deep breath and think of it in the simplest possible terms. '' IS a string, its one I use fairly regularly infact. But it does not contain a space. 
         
 string1spacecheck = contains_spaces(string1)
 print string1spacecheck
@@ -78,6 +83,10 @@ print string4spacecheck
 ##### WHY I SUCK FOR ASCII ART: <Answer Me. I see you ignorin' me. > I ran out of ideas! 
 ##### badString5 = "TODO"
 
+# Leah: Valid :) I'll give you a few ideas to think on
+# 1: What would you do if a string was empty?
+# 2: What if what is input isnt a string at all?
+# 3: Are their other ways a string can be "empty"? Hint, think about concepts (some of which have shown up in python already...) like null, none, void, nada, kaput. Is a='' the sane as a=None?
 # ========================
 
 #Leah shows strings vs booleans
@@ -85,10 +94,11 @@ print string4spacecheck
 imABoolean = True
 imAString = "True"
 
-def return_data_type(var): #Question about this: here you are using a print statement. Why doesn't this function print none?
+def return_data_type(var): 
     print type(var)
     
 return_data_type(imABoolean)
 return_data_type(imAString)
 
-
+# Question about this: here you are using a print statement. Why doesn't this function print none?
+# You missed my non obvious question. So I'm moving it down. :P
